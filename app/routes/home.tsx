74565dfa1e2bb3e089eba0b1e24088c1b4b79c0c
Joe-arch <youssef240100538@sut.edu.eg>
2025-08-04 10:55:16 +0300

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,3 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+export default function home() {
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
-export default function Home() {
-  return <Welcome />;
 }
